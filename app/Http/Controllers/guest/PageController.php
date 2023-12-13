fdee982a70049c404a39e571f9dc867e99ccbebf
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2023-12-13 17:14:12 +0100

    Print all trains

--- app/Http/Controllers/guest/PageController.php
+++ app/Http/Controllers/guest/PageController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Train;
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view('guest.index');
+        $trains = Train::all();
+        return view('guest.index', compact('trains'));
     }
 }
--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -4,12 +4,28 @@
 
     <div class="container">
         <div class="row row-gap-3">
-            {{-- @foreach($movies as $movie)
+            @foreach($trains as $train)
                 <div class="col-3">
-                    @include('partials.card')
+                    <div class="card" style="width: 18rem;">
+                        <ul class="list-group list-group-flush">
+                          <li class="list-group-item">{{ $train->azienda }}</li>
+                          <li class="list-group-item">Da: {{ $train->stazione_di_partenza }}</li>
+                          <li class="list-group-item">Dove: {{ $train->stazione_di_arrivo }}</li>
+                          <li class="list-group-item">Orario di partenza: {{ $train->orario_di_partenza }}</li>
+                          <li class="list-group-item">Orario di arrivo: {{ $train->orario_di_arrivo }}</li>
+                          <li class="list-group-item">Fermate: {{ $train->ha_fermate }}</li>
+                          <li class="list-group-item">Binario di arrivo: {{ $train->binario_arrivo }}</li>
+                          <li class="list-group-item">Binario di partenza: {{ $train->binario_partenza }}</li>
+                          <li class="list-group-item">Codice di treno: {{ $train->codice_treno }}</li>
+                          <li class="list-group-item">Carozze: {{ $train->numero_carrozze }}</li>
+                          <li class="list-group-item">Ritardo: {{ $train->in_orario }}</li>
+                          <li class="list-group-item">Cancellato: {{ $train->cancellato }}</li>
+                          <li class="list-group-item">Wifi: {{ $train->wifi }}</li>
+                          <li class="list-group-item">Bar: {{ $train->bar }}</li>
+                        </ul>
+                      </div>
                 </div>
-            @endforeach --}}
-            <H1>Ciao</H1>
+            @endforeach
         </div>
     </div>  
 
