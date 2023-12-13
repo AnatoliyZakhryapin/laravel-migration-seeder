11bdc2b20c4b20b826cc3fc911748989b7ac7154
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2023-12-13 16:52:13 +0100

    create index.blade.php

---
+++ resources/views/guest/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+        <div class="row row-gap-3">
+            {{-- @foreach($movies as $movie)
+                <div class="col-3">
+                    @include('partials.card')
+                </div>
+            @endforeach --}}
+            <H1>Ciao</H1>
+        </div>
+    </div>  
+
+@endsection
