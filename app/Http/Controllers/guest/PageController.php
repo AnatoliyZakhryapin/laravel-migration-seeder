12c65a3cbed7b55a232e6ba17845cebbfc0bfe7a
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2023-12-14 10:26:51 +0100

    Changed query to PageController

--- app/Http/Controllers/guest/PageController.php
+++ app/Http/Controllers/guest/PageController.php
@@ -10,7 +10,9 @@ class PageController extends Controller
 {
     public function index()
     {
-        $trains = Train::all();
+        // $trains = Train::all();
+        // $trains = Train::where('orario_di_partenza', '>', now())->get();
+        $trains = Train::where('orario_di_partenza', '>', now())->orderBy('orario_di_partenza', 'asc')->get();
         return view('guest.index', compact('trains'));
     }
 }
